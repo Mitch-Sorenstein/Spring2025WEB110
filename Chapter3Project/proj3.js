c1b0c11fdf3ec01d004c8b3681afb2426db5d4f1
Mitch Sorenstein <therealmuscleman1@gmail.com>
2025-03-30 21:51:03 -0500

    Add files via upload

---
+++ Chapter3Project/proj3.js
@@ -0,0 +1,24 @@
+// Author: Isaiah Stranathan
+// Date: 03/30/25
+
+let fullName = "Your Name";
+console.log("Full Name:", Isaiah Johnson Stranathan);
+
+let desiredSalary = 111193;
+console.log("Desired Salary:", desiredSalary);
+
+let isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+let friends = ["Mordecai", "Michael", "Benson"];
+console.log("Friends:", friends);
+
+let friendsSalaries = [92000, 85050, 95300];
+console.log("Friends' Salaries:", friendsSalaries);
+
+let anotherFriend = {
+    firstName: "Skips",
+    lastName: "Miller",
+    desiredSalary: 115329
+};
+console.log("Another Friend's Info:", anotherFriend);
